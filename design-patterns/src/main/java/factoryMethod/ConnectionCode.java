02e129c824afdbe9372e8c6ec9cad423cecc47a1
Sahan Vithanage <43994561+sahanmvs@users.noreply.github.com>
2022-06-06 09:56:02 +0530

    Delete design-patterns directory

--- design-patterns/src/main/java/factoryMethod/ConnectionCode.java
+++
@@ -1,6 +0,0 @@
-package factoryMethod;
-
-public enum ConnectionCode {
-    FibreDoublePay,
-    FibreTriplePay
-}
--- design-patterns/src/main/java/factoryMethod/FibreDoublePay.java
+++
@@ -1,9 +0,0 @@
-package factoryMethod;
-
-public class FibreDoublePay extends Connection{
-    @Override
-    protected void getConnection() {
-        services.add(new Telephone(2500));
-        services.add(new Broadband(10000));
-    }
-}
